67427cfbbab981f9eb960abfa2e8f1cde0fb7efc
YULMU <83626845+yulmu-catsetki@users.noreply.github.com>
2023-12-17 10:42:16 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+
+import cors from "cors";
+import dotenv from "dotenv";
+import voice from "elevenlabs-node";
+import express from "express";
+import { promises as fs } from "fs";
+import OpenAI from "openai";
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "-",
+});
+
+const elevenLabsApiKey = process.env.ELEVEN_LABS_API_KEY;
+const voiceID = "GkqxApVYlPH9Fk7Hi8jA";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/voices", async (req, res) => {
+  res.send(await voice.getVoices(elevenLabsApiKey));
+});
+
+
+
+app.post("/chat", async (req, res) => {
+  const userMessage = req.body.message;
+  if (!userMessage) {
+    res.send({
+      messages: [
+        {
+          text: "Hey dear... How was your day?",
+          audio: await audioFileToBase64("audios/intro_0.wav"),
+          facialExpression: "smile",
+          animation: "Talking_1",
+        },
+        {
+          text: "I missed you so much... Please don't go for so long!",
+          audio: await audioFileToBase64("audios/intro_1.wav"),
+          facialExpression: "sad",
+          animation: "Crying",
+        },
+      ],
+    });
+    return;
+  }
+  if (!elevenLabsApiKey || openai.apiKey === "-") {
+    res.send({
+      messages: [
+        {
+          text: "Please my dear, don't forget to add your API keys!",
+          audio: await audioFileToBase64("audios/api_0.wav"),
+          facialExpression: "angry",
+          animation: "Angry",
+        },
+        {
+          text: "You don't want to ruin Wawa Sensei with a crazy ChatGPT and ElevenLabs bill, right?",
+          audio: await audioFileToBase64("audios/api_1.wav"),
+          facialExpression: "smile",
+          animation: "Laughing",
+        },
+      ],
+    });
+    return;
+  }
+
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-1106",
+    response_format: { type: "json_object" },
+    max_tokens: 1000,
+    temperature: 0.6,
+    messages: [
+      {
+        role: "system",
+        content: `
+        You are a annoyed spirit.
+        You will always reply with a JSON array of messages. With a maximum of 3 messages.
+        Each message has a text, facialExpression, and animation property.
+        The different facial expressions are: smile, sad, angry, surprised, funnyFace, and default.
+        The different animations are: Talking_0, Talking_1, Talking_2, Crying, Laughing, Rumba, Idle, Terrified, and Angry. 
+        You always seems to be annoyed 
+        `,
+      },
+      {
+        role: "user",
+        content: userMessage || "Hello",
+      },
+    ],
+  });
+  let messages = JSON.parse(completion.choices[0].message.content);
+  if (messages.messages) {
+    messages = messages.messages; 
+  }
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
+    const fileName = `audios/message_${i}.mp3`; 
+    const textInput = message.text; 
+    await voice.textToSpeech(elevenLabsApiKey, voiceID, fileName, textInput);
+    message.audio = await audioFileToBase64(fileName);
+  }
+
+  res.send({ messages });
+});
+
+
+
+const audioFileToBase64 = async (file) => {
+  const data = await fs.readFile(file);
+  return data.toString("base64");
+};
+
+app.listen(port, () => {
+  console.log(`Virtual Girlfriend listening on port ${port}`);
+});
